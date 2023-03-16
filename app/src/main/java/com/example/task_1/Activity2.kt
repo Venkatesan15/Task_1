590f7ea12fdbfd3e4cea4fbf133e514973d39281
Venkatesan15 <jvenkat015@gmail.com>
2023-03-16 16:20:41 +0530

    first

---
+++ app/src/main/java/com/example/task_1/Activity2.kt
@@ -0,0 +1,60 @@
+package com.example.task_1
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.text.DecimalFormat
+
+class Activity2 : AppCompatActivity() {
+
+    lateinit var eNum1: EditText
+    lateinit var eNum2: EditText
+    lateinit var resBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+
+        supportActionBar?.title = "Activity 2"
+        val process = intent.getStringExtra("process").toString()
+        eNum1 = findViewById(R.id.eNum1)
+        eNum2 = findViewById(R.id.eNum2)
+        resBtn = findViewById(R.id.resBtn)
+        resBtn.text = process
+
+        resBtn.setOnClickListener {
+            getResult(process)
+        }
+
+    }
+    private fun getResult(process: String) {
+
+        if (eNum1.text.toString().trim().isNotEmpty() && eNum2.text.toString().trim().isNotEmpty()) {
+            val i = Intent()
+            val num1: Float = eNum1.text.toString().toFloat()
+            val num2: Float = eNum2.text.toString().toFloat()
+
+            val ans = when(process) {
+                "Add" -> (num1 + num2)
+                "Subtract" -> (num1 - num2)
+                "Multiply" -> (num1 * num2)
+                "Division" -> (num1/num2)
+                else -> null!!
+            }
+
+            val format = DecimalFormat("0.#")
+            i.putExtra("num1", format.format(num1))
+            i.putExtra("num2", format.format(num2))
+            i.putExtra("ans", format.format(ans))
+            i.putExtra("action", process)
+            setResult(RESULT_OK, i)
+            finish()
+        }
+        else {
+            Toast.makeText(this,"Enter valid Input",Toast.LENGTH_SHORT).show()
+            setResult(RESULT_CANCELED,null)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/task_1/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.task_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var addBtn: Button
+    lateinit var subBtn: Button
+    lateinit var multiplyBtn: Button
+    lateinit var divBtn: Button
+    lateinit var resetBtn: Button
+    lateinit var res: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Activity 1"
+        declaration()
+        addBtn.setOnClickListener(this)
+        subBtn.setOnClickListener(this)
+        multiplyBtn.setOnClickListener(this)
+        divBtn.setOnClickListener(this)
+        resetBtn.setOnClickListener(this)
+    }
+
+    private fun declaration() {
+        addBtn = findViewById(R.id.addBtn)
+        subBtn = findViewById(R.id.subBtn)
+        multiplyBtn = findViewById(R.id.mulBtn)
+        divBtn = findViewById(R.id.divBtn)
+        resetBtn = findViewById(R.id.resetBtn)
+        res = findViewById(R.id.textView)
+    }
+
+
+    override fun onClick(v: View?) {
+        val i = Intent(this,Activity2::class.java)
+        when(v) {
+            addBtn -> {
+                i.putExtra("process",addBtn.text.toString())
+                resultLaunch.launch(i)
+            }
+            subBtn -> {
+                i.putExtra("process",subBtn.text.toString())
+                resultLaunch.launch(i)
+            }
+            multiplyBtn -> {
+                i.putExtra("process",multiplyBtn.text.toString())
+                resultLaunch.launch(i)
+            }
+            divBtn -> {
+                i.putExtra("process",divBtn.text.toString())
+                resultLaunch.launch(i)
+            }
+            resetBtn -> {
+                addBtn.visibility = View.VISIBLE
+                subBtn.visibility = View.VISIBLE
+                multiplyBtn.visibility = View.VISIBLE
+                divBtn.visibility = View.VISIBLE
+                resetBtn.visibility = View.GONE
+                res.visibility = View.GONE
+            }
+        }
+    }
+
+    private var resultLaunch = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        receiveResult(it)
+    }
+    private fun receiveResult(activityResult: ActivityResult) {
+        val num1: String?
+        val num2: String?
+        val ans: String?
+        val action: String?
+
+        if (activityResult.resultCode == RESULT_OK) {
+            val data: Intent? = activityResult.data
+            num1 = data?.getStringExtra("num1")
+            num2 = data?.getStringExtra("num2")
+            ans = data?.getStringExtra("ans")
+            action = data?.getStringExtra("action")
+
+            addBtn.visibility = View.GONE
+            subBtn.visibility = View.GONE
+            multiplyBtn.visibility = View.GONE
+            divBtn.visibility = View.GONE
+            resetBtn.visibility = View.VISIBLE
+            res.visibility = View.VISIBLE
+
+            res.text = "Your Result is $ans for inputs $num1 and $num2 with action $action"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("Save", res.text.toString())
+        outState.putBoolean("Visible", res.visibility == View.VISIBLE)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.getBoolean("Visible") ) {
+            addBtn.visibility = View.GONE
+            subBtn.visibility = View.GONE
+            multiplyBtn.visibility = View.GONE
+            divBtn.visibility = View.GONE
+            resetBtn.visibility = View.VISIBLE
+            res.visibility = View.VISIBLE
+            res.text = savedInstanceState.getString("Save")
+        }
+    }
+
+}
